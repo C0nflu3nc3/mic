9e6c8f9d92498ea9d4543ea5c5f4d02a006d3f34
r.gorkovenko <rgorkovenko@gmail.com>
2023-05-29 23:09:55 +0600

    auth form styles, teams table1 styles

--- teams.php
+++ teams.php
@@ -39,67 +39,76 @@ else header('Location: /');
 
 <head>
     <meta charset="UTF-8">
-    <title>Авторизация и регистрация</title>
+    <title>Команда</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <div>
-        <?= $username?>
-        <a href="vendor/logout.php" class="logout">Выход</a>
-    </div>
-    <div>
-        <table width="100%" style="margin 50px 0" height="50">
-            <tr>
-                <th colspan="5">Счёт твоей команды</th>
-            </tr>
-            <tr>
-                <th style="width: 100px;">Название команды:</th>
-                <th style="width: 100px;">Очки:</th>
-            </tr>
-            <?php
-            while($data = mysqli_fetch_array($queryScore,MYSQLI_ASSOC)){
-                ?>
-            <tr>
-                <td><?= $data['Name']?></td>
-                <td><?= $data['Scores']?></td>
-            </tr>
-            <?php
-            }   
-        ?>
-        </table>
-    </div>
-
-    <div>
-        <table width="100%" style="margin 50px 0" height="50">
-            <tr>
-                <th colspan="5">Операции</th>
-            </tr>
-            <tr>
-                <th style="width: 100px;">Период:</th>
-                <th style="width: 100px;">Название команды:</th>
-                <th style="width: 100px;">Очки:</th>
-                <th style="width: 100px;">Комментарий:</th>
-            </tr>
-            <?php
-            while($data = mysqli_fetch_array($queryOperation,MYSQLI_ASSOC)){
-                ?>
-            <tr>
-                <td><?= $data['Period']?></td>
-                <td><?= $data['Name']?></td>
-                <td><?= $data['Score']?></td>
-                <td><?= $data['Comment']?></td>
-            </tr>
-            <?php
-            }   
-        ?>
-        </table>
-    </div>
-        <div>
-        <form action="send1.php">
-        <button type="submit">Отправить деньги</button>
+    <div class='container-fluid text-center'>
+        <div class="row header">
+            <?= $username?>
+            <a href="vendor/logout.php" class="logout">Выход</a>
         </div>
+        <div class="row content">
+            <div>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th colspan="5">Счёт твоей команды</th>
+                        </tr>
+                        <tr>
+                            <th>Название команды:</th>
+                            <th>Очки:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            while($data = mysqli_fetch_array($queryScore,MYSQLI_ASSOC)){
+                                ?>
+                                <tr>
+                                    <td><?= $data['Name']?></td>
+                                    <td><?= $data['Scores']?></td>
+                                </tr>
+                                <?php
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
 
+            <div>
+                <table>
+                    <tr>
+                        <th colspan="5">Операции</th>
+                    </tr>
+                    <tr>
+                        <th>Период:</th>
+                        <th>Название команды:</th>
+                        <th>Очки:</th>
+                        <th>Комментарий:</th>
+                    </tr>
+                    <?php
+                    while($data = mysqli_fetch_array($queryOperation,MYSQLI_ASSOC)){
+                        ?>
+                    <tr>
+                        <td><?= $data['Period']?></td>
+                        <td><?= $data['Name']?></td>
+                        <td><?= $data['Score']?></td>
+                        <td><?= $data['Comment']?></td>
+                    </tr>
+                    <?php
+                    }
+                ?>
+                </table>
+            </div>
+                <div>
+                <form action="send1.php">
+                <button class="btn btn-primary" type="submit">Отправить деньги</button>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
