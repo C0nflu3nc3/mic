b0d43e1e53f100e79452a736fba6b69d014970e3
swd <swd1986@yandex.ru>
2023-06-05 17:23:59 +0600

    fix bugs

--- api/get_users.php
+++ api/get_users.php
@@ -1,11 +1,11 @@
 <?php
-    $sql = "SELECT login FROM users";
+    $sql = "SELECT id, login FROM users";
     $result = mysqli_query($connect, $sql);
 if ($result) {
     // Process the retrieved data
     while ($row = mysqli_fetch_assoc($result)) {
         // Access individual fields using $row['field_name']
-        echo '<option>' . $row['login'] . '</option>';
+        echo '<option value="'. $row['id'] .'">' . $row['login'] . '</option>';
     }
 } else {
     echo "Error executing the query: " . mysqli_error($connect);
